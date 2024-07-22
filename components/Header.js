018467257524c2e22b65ec9d08b81aa7b6a26b48
pikaqi-dot <hual83129@gmail.com>
2024-07-22 12:41:17 +0800

    圆形头像

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,25 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import touxiang from './touxiang.png'
+import touxiang from './touxiang.png';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-        <Image
-          src={touxiang}
-          // alt="Avatar"
-          // className={styles.avatar}
-          width={100}
-          height={100}
-        />
+      <Image
+        src={touxiang}
+        alt="Avatar"
+        style={{
+          'border-radius': '50%',
+          overflow: 'hidden',
+          width: '100px',
+          height: '100px'
+        }}
+        width={100}
+        height={100}
+      />
       <p className="text-1xl text-center dark:text-white">
         {name}
       </p>
-      <p className="text-2xl text-left dark:text-white">
+      <p className="text-2xl text-center dark:text-white">
         <Link href="/">回到首页</Link>
       </p>
 
