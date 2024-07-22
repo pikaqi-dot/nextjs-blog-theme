46f9d612ac9158e5ca33e0352767bf288e44ef13
pikaqi-dot <hual83129@gmail.com>
2024-07-22 12:22:59 +0800

    方块头像

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,16 @@
 import Link from 'next/link';
-
+import Image from 'next/image';
+import touxiang from './touxiang.png'
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-gradient-conic from-gradient-3 to-gradient-4" />
+        <Image
+          src={touxiang}
+          // alt="Avatar"
+          // className={styles.avatar}
+          width={100}
+          height={100}
+        />
       <p className="text-1xl text-center dark:text-white">
         {name}
       </p>
